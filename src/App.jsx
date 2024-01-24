b9201c362533678f97baee2cdf2376fe828cb436
CarlosSimon02 <caloycamacho02@gmail.com>
2024-01-24 16:09:01 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        return response.json();
+      })
+      .then((countries) => {
+        console.log(countries);
+        setCountries(countries);
+      });
+  }, []);
+
+  return (
+    <div>
+      <input type="search" />
+      <div>
+        {countries.map((country,index) => {
+          return (
+            <div key={country.cca3}>
+              <h1>{country.name.common}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
