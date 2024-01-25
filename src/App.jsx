3b85e3d4ea4fd9fea9c1584f3ab927cd29e4be2e
CarlosSimon02 <caloycamacho02@gmail.com>
2024-01-25 19:51:56 +0800

    Add country card style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [countries, setCountries] = useState([]);
-
+  useEffect()
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => {
