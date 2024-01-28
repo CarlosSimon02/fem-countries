e0ec515651e04d7bc6ab0b61c5536554ef2db326
CarlosSimon02 <caloycamacho02@gmail.com>
2024-01-28 18:16:00 +0800

    Destructure data from currentCountry

--- src/routes/country/country.route.jsx
+++ src/routes/country/country.route.jsx
@@ -1,5 +1,51 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { CountriesContext } from "../../contexts/countries.context";
+
 const Country = () => {
-  return <div>This is the country route</div>
-}
+  const { countries } = useContext(CountriesContext);
+  const { country } = useParams();
+  const [currentCountry, setCurrentCountry] = useState(null);
+
+  useEffect(() => {
+    const foundCountry = countries.find(({ name }) => {
+      const formattedName = name.common.toLowerCase().replace(/ /g, "-");
+      return formattedName === country;
+    });
+
+    setCurrentCountry(foundCountry || null);
+  }, [countries, country]);
+
+  if (!currentCountry) {
+    return <div>Loading</div>;
+  }
+
+  console.log(currentCountry);
+
+  const {
+    flags: { png: image, alt: imageAlt },
+    name: { common: name },
+    area,
+    population,
+    region,
+    subregion,
+    capital,
+    tld: topLevelDomain,
+    currencies,
+    languages,
+    borders,
+  } = currentCountry;
+
+  return <div>
+    <img src={image} alt={imageAlt} />
+    <div>
+      <p>{name}</p>
+      <p>{area}</p>
+      <p>{population}</p>
+      <p>{region}</p>
+      <p>{subregion}</p>
+    </div>
+  </div>;
+};
 
 export default Country;
